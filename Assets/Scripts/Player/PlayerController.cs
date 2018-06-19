1859ff87f89990ba83886801d4f856430cea922c
Steve <s.delgrange@rubika-edu.com>
2018-06-19 10:13:15 +0200

    Added scene. Movement/Rotation mecanisms done. Need to add animations.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
+    
+    [System.Serializable]
+    private struct SpeedTierValues
+    {
+        public float minSpeed;
+        public float maxSpeed;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public bool isGoingForward = false;
+    [Range(-1, 1)]
+    public float rotationDirection = 0;
+
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float acceleration;
+    [SerializeField] List<SpeedTierValues> speedTierValues;
+    
+    private Rigidbody _rb;
+    private int currentSpeedTier;
+    private float currentMinSpeed;
+    private float currentMaxSpeed;
+    private float currentSpeed;
+
+    void Start () {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null) {
+            _rb = gameObject.AddComponent<Rigidbody>();
+        }
+
+        currentSpeedTier = 0;
+        if (speedTierValues.Count > 0) {
+            currentMinSpeed = speedTierValues[0].minSpeed;
+            currentMaxSpeed = speedTierValues[0].maxSpeed;
+        } else {
+            currentMinSpeed = 0;
+            currentMaxSpeed = 0;
+        }
+
+        currentSpeed = 0;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate ()
+    {
+        Rotate();
+
+        CalculateSpeed();
+
+        Move();
+    }
+
+    private void Rotate()
+    {
+        if(rotationDirection != 0) {
+            float newY = transform.eulerAngles.y + (rotationDirection * rotationSpeed * Time.deltaTime);
+            _rb.MoveRotation(Quaternion.Euler(0, newY, 0));
+        }
+    }
+
+    private void CalculateSpeed()
+    {
+        if (isGoingForward){
+            currentSpeed += acceleration = Time.deltaTime;
+        } else {
+            currentSpeed -= acceleration = Time.deltaTime;
+        }
+
+        currentSpeed = Mathf.Clamp(currentSpeed, currentMinSpeed, currentMaxSpeed);
+    }
+
+    private void Move()
+    {
+        if(currentSpeed != 0) {
+            _rb.MovePosition(transform.position + transform.forward * currentSpeed * Time.deltaTime);
+        }
+    }
+
+    public void TryIncreaseSpeedTier()
+    {
+        if(currentSpeedTier < speedTierValues.Count - 1 && currentSpeedTier >= 0) {
+            if(currentSpeed == currentMaxSpeed) {
+                currentSpeedTier++;
+                currentMinSpeed = speedTierValues[currentSpeedTier].minSpeed;
+                currentMaxSpeed = speedTierValues[currentSpeedTier].maxSpeed;
+            }
+        }
+    }
+
+    public void TryDecreaseSpeedTier()
+    {
+        if (currentSpeedTier < speedTierValues.Count && currentSpeedTier > 0) {
+            if (currentSpeed == currentMinSpeed) {
+                currentSpeedTier--;
+                currentMinSpeed = speedTierValues[currentSpeedTier].minSpeed;
+                currentMaxSpeed = speedTierValues[currentSpeedTier].maxSpeed;
+            }
+        }
+    }
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerController))]
 public class PlayerInput : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private PlayerController _controller;
+
+    private bool incPressed = false;
+    private bool decPressed = false;
+
+    void Start () {
+        _controller = GetComponent<PlayerController>();
+        if(_controller == null)
+        {
+            _controller = gameObject.AddComponent<PlayerController>();
+        }
+    }
 	
-	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetAxis("Forward") > 0) {
+            _controller.isGoingForward = true;
+        } else {
+            _controller.isGoingForward = false;
+        }
+
+        _controller.rotationDirection = Input.GetAxis("Horizontal");
+
+        if (Input.GetAxisRaw("IncreaseSpeedTier") == 1 && !incPressed)
+        {
+            _controller.TryIncreaseSpeedTier();
+            incPressed = true;
+        }
+        else if(Input.GetAxisRaw("IncreaseSpeedTier") == 0)
+        {
+            incPressed = false;
+        }
+
+        if (Input.GetAxisRaw("DecreaseSpeedTier") == 1 && !decPressed)
+        {
+            _controller.TryDecreaseSpeedTier();
+            decPressed = true;
+        }
+        else if (Input.GetAxisRaw("DecreaseSpeedTier") == 0)
+        {
+            decPressed = false;
+        }
+
+    }
 }
